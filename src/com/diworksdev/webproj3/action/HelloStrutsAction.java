f22c0977e22ff611ad8554b1db0d9788f70df6d6
ibukiyabu <ibuki.y2115@gmail.com>
2024-02-18 18:59:00 +0900

    一部修正

--- src/com/diworksdev/webproj3/action/HelloStrutsAction.java
+++ src/com/diworksdev/webproj3/action/HelloStrutsAction.java
@@ -4,32 +4,31 @@ import com.diworksdev.webproj3.dao.HelloStrutsDAO;
 import com.diworksdev.webproj3.dto.HelloStrutsDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HelloStrutsAction extends ActionSupport{
-	
+public class HelloStrutsAction extends ActionSupport {
 	private String result;
-	
+
 	public String execute() {
 		String ret = ERROR;
+		
 		HelloStrutsDAO dao = new HelloStrutsDAO();
 		HelloStrutsDTO dto = new HelloStrutsDTO();
-		
+	
 		dto = dao.select();
-		
 		System.out.println(dto.getResult());
 		result = dto.getResult();
 		
-		if(result.equals("MySQLと接続できます。")) {
+		if (result.equals("MySQL と接続できます。")) {
 			ret = SUCCESS;
-		}else {
+		} else {
 			ret = ERROR;
 		}
 		return ret;
 	}
-	
+
 	public String getResult() {
 		return result;
 	}
-	
+
 	public void setResult(String result) {
 		this.result = result;
 	}
